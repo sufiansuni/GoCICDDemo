878e1a7cd440d760f3c2170054bc157376019578
Sufian Suni <85665908+sufiansuni@users.noreply.github.com>
2021-06-11 15:12:06 +0800

    Update calc.go

--- calc.go
+++ calc.go
@@ -2,7 +2,7 @@ package calc
 
 // Add two numbers and return the result.
 func Add(val1, val2 int) int {
-	return val1 + val2 + 1
+	return val1 + val2
 }
 
 // Subtract two numbers and return the result.
